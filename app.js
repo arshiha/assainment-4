a6412d0c6af1aa1d4c6607abf7055dcd11f8b564
Arshiha Bristy <96794207+arshiha@users.noreply.github.com>
2022-02-17 19:59:39 +0600

    Assignment done

---
+++ app.js
@@ -0,0 +1,25 @@
+// handle calculate button 
+document.getElementById('calculate-button').addEventListener('click', function () {
+  calculate()
+
+})
+//income filed 
+function calculate() {
+  const incomeInput = document.getElementById("income-filed").value;
+  const incomeText = parseFloat(incomeInput);
+  // console.log(incomeText);
+
+  // expense
+
+  const foodInput = document.getElementById('food-filed').value;
+  const rentInput = document.getElementById('rent-filed').value;
+  const clothesInput = document.getElementById('clothes-filed').value;
+  // calculate
+  const calculateTotal =
+    parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothesInput);
+  ///calculate balance Total
+  const balanceTotal = incomeText - calculateTotal;
+  document.getElementById('balance').innerText = balanceTotal;
+  document.getElementById("total-expense").innerText = calculateTotal;
+
+}
